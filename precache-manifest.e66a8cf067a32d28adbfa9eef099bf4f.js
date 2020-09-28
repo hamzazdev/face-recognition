cb354f753f52471b1da298a491c42acf65e0aedf
Hamza Abdalmajeed <hamza@Hamzas-MacBook-Pro-2.local>
2020-09-28 17:17:54 +0100

    Updates

---
+++ precache-manifest.e66a8cf067a32d28adbfa9eef099bf4f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58936f0af475bcce7f20d7ed06df7cae",
+    "url": "/face-recognition/index.html"
+  },
+  {
+    "revision": "0d7f5aff708315a47404",
+    "url": "/face-recognition/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "6a3ed1aa2a2e27e9a929",
+    "url": "/face-recognition/static/css/main.88a28213.chunk.css"
+  },
+  {
+    "revision": "0d7f5aff708315a47404",
+    "url": "/face-recognition/static/js/2.8db51893.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/face-recognition/static/js/2.8db51893.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a3ed1aa2a2e27e9a929",
+    "url": "/face-recognition/static/js/main.72755f27.chunk.js"
+  },
+  {
+    "revision": "b86f4757c9b31828b647",
+    "url": "/face-recognition/static/js/runtime-main.9817fe0c.js"
+  }
+]);
